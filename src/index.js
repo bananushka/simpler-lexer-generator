13727b01e8e4a0d2df1685648d6c30a0a21086ad
bananushka <bananushka@gmail.com>
2015-12-28 21:29:28 +0200

    Basic functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,130 @@
 export default class Generator {
-  constructor() {
-    
+  constructor(grammar) {
+    this.grammar = grammar;
+    this.rules = [];
   }
+
+  generateRules() {
+    return this.grammar
+            .split('\n')
+            .reverse()
+            .filter(line => line.trim().length > 0)
+            .filter(line => !line.match(/^#/))
+            .map(line => line.split('::=').map(x => x.trim()))
+            .map(([name, rule]) => {
+              return {name, rule};
+            })
+            .map(({name, rule}) => {
+              this.rules.push({
+                name,
+                rule: this.translateRule(rule)
+              });
+              return this.rules[this.rules.length - 1];
+            })
+            .map(({name, rule}) => {
+              return {name, rule: new RegExp(rule)};
+            })
+            .filter(rule => !rule.name.match(/^aux:/));
+  }
+  translateRule(rule) {
+    switch (true) {
+    case compoundRule(rule):    return this.translateCompoundRule(rule);
+    case repetitionRule(rule):  return this.translateRepetitionRule(rule);
+    case optionalRule(rule):    return this.translateOptionalRule(rule);
+    case referenceRule(rule):   return this.translateReferenceRule(rule);
+    case terminalRule(rule):    return this.translateTerminalRule(rule);
+    default: return '';
+    }
+  }
+  translateTerminalRule(rule) {
+    return '[' + rule
+                  .split(' | ')
+                  .map(terminal)
+                  .join('')
+            + ']';
+  }
+  translateReferenceRule(rule) {
+      return '(' + rule
+                    .split(' | ')
+                    .map(reference.bind(null, this.rules))
+                    .join('|')
+            + ')';
+  }
+  translateOptionalRule(rule) {
+    return this.translateReferenceRule(rule.substr(1, rule.length - 2).trim()) + '?';
+  }
+  translateRepetitionRule(rule) {
+    return this.translateReferenceRule(rule.substr(1, rule.length - 2).trim()) + '*';
+  }
+  translateCompoundRule(rule) {
+    return '(' +
+              rule
+                .split(' | ')
+                .map(rulePart => rulePart
+                                  .split(', ')
+                                  .map(rulePartSeq => this.translateRule(rulePartSeq))
+                                  .join('')
+                    )
+                .join('|')
+            + ')';
+  }
+}
+
+function terminalRule(rule) {
+  return rule.split(' | ').every(isTerminal);
+}
+function isTerminal(rulePart) {
+  return isSimpleTerminal(rulePart) || isRange(rulePart);
+}
+function isSimpleTerminal(rulePart) {
+  return !!rulePart.match(/^'.+'$/);
+}
+function isRange(rulePart) {
+  return rulePart == '...';
+}
+
+function terminal(rulePart) {
+  return rulePart == '...' ? '-' :
+          rulePart
+          .substr(1, rulePart.length - 2)
+          .replace('-', '\\-')
+          .replace('/', '\\/');
+}
+
+function referenceRule(rule) {
+  return rule.split(' | ').some(isReference);
+}
+function isReference(rulePart) {
+  return isSimpleRefernce(rulePart);
+}
+function isSimpleRefernce(rulePart) {
+  return !!rulePart.match(/^[a-zA-Z0-9_-\s]+$/);
+}
+function reference(references, rulePart) {
+  if (isSimpleTerminal(rulePart)) {
+    return terminal(rulePart);
+  } else {
+    return references.find(reference => reference.name == rulePart || reference.name == 'aux:' + rulePart).rule;
+  }
+}
+
+function optionalRule(rule) {
+  return rule.split(' | ').some(isOptional);
+}
+function isOptional(rulePart) {
+  return !!rulePart.match(/^\[.+\]$/);
+}
+
+function repetitionRule(rule) {
+  return rule.split(' | ').some(isRepetition);
+}
+function isRepetition(rulePart) {
+  return !!rulePart.match(/^\{.+\}$/);
+}
+
+function compoundRule(rule) {
+  return rule.split(' | ').some(isCompound);
+}
+function isCompound(rulePart) {
+  return !isTerminal(rulePart) && rulePart.includes(',');
 }
--- test/Generator.spec.js
+++ test/Generator.spec.js
@@ -1,10 +1,119 @@
 import assert from 'assert';
-import Generator from '../lib';
+import Generator from '../';
 
 describe('Generator', () => {
   describe('#constructor()', () => {
     it('should create a new Generator object', () => {
       assert(new Generator instanceof Generator);
     });
+    it('should create an object with a generateRules method', () => {
+      assert((new Generator).generateRules instanceof Function);
+    })
+  });
+
+  const testCases = [
+    {
+      title: 'should work for simple terminals',
+      grammar: `equals ::= '='`,
+      expected: [{
+        name: 'equals',
+        rule: /[=]/
+      }]
+    },
+    {
+      title: 'should work for multiple terminals',
+      grammar: `vowel ::= 'a' | 'e' | 'i' | 'o' | 'u'`,
+      expected: [{
+        name: 'vowel',
+        rule: /[aeiou]/
+      }]
+    },
+    {
+      title: 'should work with ranges',
+      grammar: `lower case letter ::= 'a' | ... | 'z'`,
+      expected: [{
+        name: 'lower case letter',
+        rule: /[a-z]/
+      }]
+    },
+    {
+      title: 'should work with combination of the above',
+      grammar: `alpha numeric underscore ::= 'a' | ... | 'z' | 'A' | ... | 'Z' | '0' | ... | '9' | '_'`,
+      expected: [{
+        name: 'alpha numeric underscore',
+        rule: /[a-zA-Z0-9_]/
+      }]
+    },
+    {
+      title: 'should work with (non-recursive) forward-references',
+      grammar: `
+        digit ::= digit without zero | '0'
+        digit without zero ::= '1' | ... | '9'
+      `,
+      expected: [
+        {
+          name: 'digit without zero',
+          rule: /[1-9]/
+        },
+        {
+          name: 'digit',
+          rule: /([1-9]|0)/
+        }
+      ]
+    },
+    {
+      title: 'should work with optionals',
+      grammar: `zero or nothing ::= [ '0' ]`,
+      expected: [{
+        name: 'zero or nothing',
+        rule: /(0)?/
+      }]
+    },
+    {
+      title: 'should work with repetitions',
+      grammar: `
+        binary or nothing ::= { binary digit }
+        binary digit ::= '0' | '1'
+      `,
+      expected: [
+        {
+          name: 'binary digit',
+          rule: /[01]/
+        },
+        {
+          name: 'binary or nothing',
+          rule: /([01])*/
+        }
+      ]
+    },
+    {
+      title: 'should work with compound rules',
+      grammar: `
+        natural number ::= non-zero digit, { digit }
+        digit ::= '0' | non-zero digit
+        non-zero digit ::= '1' | ... | '9'
+      `,
+      expected: [
+        {
+          name: 'non-zero digit',
+          rule: /[1-9]/
+        },
+        {
+          name: 'digit',
+          rule: /(0|[1-9])/
+        },
+        {
+          name: 'natural number',
+          rule: /(([1-9])((0|[1-9]))*)/
+        }
+      ]
+    }
+  ];
+  describe('#generateRules()', () => {
+    for (let {title, grammar, expected} of testCases) {
+      it(title, () => {
+        assert.deepEqual((new Generator(grammar)).generateRules(), expected);
+      });
+    }
   });
 });
